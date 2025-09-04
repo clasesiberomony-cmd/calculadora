6cab785842481850172dc2a1a34290037e62c9e3
Alejandro Montes Moreno <alejandromontesmoreno@gmail.com>
2025-09-04 13:33:59 -0600

    Version 1

---
+++ README.txt
@@ -0,0 +1,3 @@
+Realizar la programación de la raiz cuadrada en un metodo de la 
+clase Avanzadas
+Realizar Pull request al proyecto original
---
+++ clases/avanzadas.py
@@ -0,0 +1,15 @@
+class Avanzadas:
+    def __init__(self):
+        self.numero1 = 0
+        self.numero2 = 0
+        self.potencia = 0
+
+    def leerNumeros(self):
+        pass
+    
+    def calcularPotencia(self):
+        self.potencia = self.numero1 ** self.numero2
+
+    def mostrarResultado(self, resultado):
+        pass
+        
